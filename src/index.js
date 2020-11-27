5b59b33b78f4b2ed3e46f6945de9ac27708a119b
KennethLG <orsonleiziaga@gmail.com>
2020-11-27 13:26:13 -0500

    babel and react ready

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("Hi men");
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+	<h1>Hi bro</h1>,
+	document.getElementById("root")
+);
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,19 @@ module.exports = {
 	},
 	plugins:[
 		new HtmlWebpackPlugin({template: './src/index.html'})
-	]
+	],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use : {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env", "@babel/preset-react"]
+					}
+				}
+			}
+		]
+	}
 }
