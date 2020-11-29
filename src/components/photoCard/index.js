55ce749613e41a0984d8aa2ba8beefe51d223236
KennethLG <orsonleiziaga@gmail.com>
2020-11-29 16:04:37 -0500

    Intersection observer navigation support added

--- src/components/photoCard/index.js
+++ src/components/photoCard/index.js
@@ -5,20 +5,26 @@ import { MdFavoriteBorder}  from "react-icons/md";
 const DEFAULT_IMAGE = "https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png";
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE}) => {
+
 	const element = useRef(null);
 	const [show, setShow] = useState(false);
 
 	useEffect(() => {
-		const observer = new window.IntersectionObserver((entries)=> {
-			const { isIntersecting } = entries[0];
-			if (isIntersecting) {
-				setShow(true);
-			} else {
-				setShow(false);
-			}
-		});
-
-		observer.observe(element.current)
+		Promise.resolve(
+			typeof window.IntersectionObserver !== "undefined"
+			? window.IntersectionObserver
+			: import("Intersection-observer")
+			).then(() => {
+				const observer = new window.IntersectionObserver((entries)=> {
+					const { isIntersecting } = entries[0];
+					if (isIntersecting) {
+						setShow(true);
+					} else {
+						setShow(false);
+					}
+				});
+				observer.observe(element.current)
+			})
 	}, [element]);
 
 	return(
