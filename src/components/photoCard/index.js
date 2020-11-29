e76504bb213655fdbf4e64ed846bf7d1dca7bc3c
KennethLG <orsonleiziaga@gmail.com>
2020-11-29 15:47:45 -0500

    intersection observer

--- src/components/photoCard/index.js
+++ src/components/photoCard/index.js
@@ -1,20 +1,42 @@
-import React from "react";
-import {ImgWrapper, Img, Button} from "./styles";
+import React, {Fragment, useEffect, useState, useRef} from "react";
+import {Article, ImgWrapper, Img, Button} from "./styles";
 import { MdFavoriteBorder}  from "react-icons/md";
 
 const DEFAULT_IMAGE = "https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png";
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE}) => {
-	return(
-		<article>
-			<a href={`/detail/${id}`}>
-				<ImgWrapper>
-					<Img src={src} />
-				</ImgWrapper>
-			</a>
 
-			<Button>
-				<MdFavoriteBorder size="32px"/>{likes} likes
-			</Button>
-		</article>
+export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE}) => {
+	const element = useRef(null);
+	const [show, setShow] = useState(false);
+
+	useEffect(() => {
+		const observer = new window.IntersectionObserver((entries)=> {
+			const { isIntersecting } = entries[0];
+			if (isIntersecting) {
+				setShow(true);
+			} else {
+				setShow(false);
+			}
+		});
+
+		observer.observe(element.current)
+	}, [element]);
+
+	return(
+		<Article ref={element}>
+			{
+				show && <Fragment> 
+					<a href={`/detail/${id}`}>
+						<ImgWrapper>
+							<Img src={src} />
+						</ImgWrapper>
+					</a>
+
+					<Button>
+						<MdFavoriteBorder size="32px"/>{likes} likes
+					</Button>
+				</Fragment>
+			}
+			
+		</Article>
 	)
 }
