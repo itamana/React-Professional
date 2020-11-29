490b423bd1c46dcc127a6b1c7430127854711420
KennethLG <orsonleiziaga@gmail.com>
2020-11-29 12:31:02 -0500

    List of photo cards added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import {Category} from "./components/category";
 import {ListOfCategories} from "./components/listOfCategories";
 import {GlobalStyle} from "./GlobalStyles";
-
+import {ListOfPhotoCards} from "./components/ListOfPhotoCards";
 export const App = () => (
 	<div>
 		<GlobalStyle />
 		<ListOfCategories/>
+		<ListOfPhotoCards />
 	</div>
 );
--- src/components/listOfCategories/index.js
+++ src/components/listOfCategories/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import {Category} from "../category";
 import {List, Item} from "./styles";
+import {categories} from "../../../api/db.json";
 
 export const ListOfCategories = () => {
 	return (
 		<List>
 			{
-				[1, 2, 3, 4].map(cat => <Item key={cat}> <Category /> </Item> )
+				categories.map(cat => <Item key={cat.id}> <Category {...cat} /> </Item> )
 			}
 		</List>
 	)
