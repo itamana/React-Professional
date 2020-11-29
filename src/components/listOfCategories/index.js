e66433f947335cb99a5ed4894c51bfccefd08dbe
KennethLG <orsonleiziaga@gmail.com>
2020-11-29 11:47:51 -0500

    css styles added with styled-components

---
+++ src/components/listOfCategories/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Category} from "../category";
+import {List, Item} from "./styles";
+
+export const ListOfCategories = () => {
+	return (
+		<List>
+			{
+				[1, 2, 3, 4].map(cat => <Item key={cat}> <Category /> </Item> )
+			}
+		</List>
+	)
+}
