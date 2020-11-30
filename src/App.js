fe93b2d8790c4cf43e3d94c22553ebaf75962531
KennethLG <orsonleiziaga@gmail.com>
2020-11-30 14:07:32 -0500

    Nabvar bottom added

--- src/App.js
+++ src/App.js
@@ -5,21 +5,20 @@ import {Logo} from "./components/Logo";
 import {PhotoCardWithQuery} from "./container/PhotoCardWithQuery";
 import {Home} from "./pages/Home";
 import {Router} from "@reach/router";
+import { Detail } from "./pages/Detail";
+import {NavBar} from "./components/NavBar";
 
 export const App = () => {
-	const urlParams = new window.URLSearchParams(window.location.search);
-	const detailId = urlParams.get("detail");
 	return (
 		<div>
 			<GlobalStyle />
-			<Logo/> {
-				detailId
-					? <PhotoCardWithQuery id={detailId}/>
-					: <Router>
-						<Home path="/"/>
-						<Home path="/pet/:id"/>
-					</Router>
-			}
+			<Logo/> 
+			<Router>
+				<Home path="/"/>
+				<Home path="/pet/:id"/>
+				<Detail path="/detail/:detailId"/>
+			</Router>
+			<NavBar />
 		</div>
 	);
 }
