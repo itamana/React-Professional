c892b234d0e57d025a836bbcd9c70048a5108c4e
KennethLG <orsonleiziaga@gmail.com>
2020-11-30 15:33:47 -0500

    react context and protected links

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import {PhotoCardWithQuery} from "./container/PhotoCardWithQuery";
 import {Home} from "./pages/Home";
 import {Router} from "@reach/router";
 import { Detail } from "./pages/Detail";
+import { Favs } from "./pages/Favs";
+import { User } from "./pages/User";
+import { NotRegisteredUser } from "./pages/NotRegisteredUser";
 import {NavBar} from "./components/NavBar";
+
+import Context from "./Context";
+
 
 export const App = () => {
 	return (
@@ -18,6 +24,22 @@ export const App = () => {
 				<Home path="/pet/:id"/>
 				<Detail path="/detail/:detailId"/>
 			</Router>
+
+			<Context.Consumer>
+				{
+					({ isAuth }) =>
+						isAuth
+							? <Router>
+								<Favs path="/favs"/>
+								<User path="/user"/>
+							</Router>
+
+							: <Router>
+								<NotRegisteredUser path="/favs"/>
+								<NotRegisteredUser path="/user"/>
+							</Router>
+				}
+			</Context.Consumer>
 			<NavBar />
 		</div>
 	);
---
+++ src/pages/NotRegisteredUser.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Context from "../Context";
+
+export const NotRegisteredUser = () => (
+	<Context.Consumer>
+		{
+			({ activateAuth }) => {
+				return (
+					<form onSubmit={activateAuth}>
+						<button> Login </button>
+					</form>
+				)
+			}
+		}
+	</Context.Consumer>
+)
