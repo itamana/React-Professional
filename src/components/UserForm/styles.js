4cbafcc3379d52607a51684307588b6ef694ee66
KennethLG <orsonleiziaga@gmail.com>
2020-11-30 22:54:33 -0500

    register

---
+++ src/components/UserForm/styles.js
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+	padding: 16px 0;
+`
+
+export const Input = styled.input`
+	border: 1px solid #ccc;
+	border-radius: 3px;
+	padding: 8px 4px;
+	display: block;
+	width: 100%;
+	left: 0;
+	right: 0;
+	margin: 0 auto;
+	$[disabled] {
+		opacity: .3;
+	}
+`
+
+export const Button = styled.button`
+	background: #8d00ff;
+	border-radius: 3px;
+	color: #fff;
+	height: 32px;
+	display: block;
+	width: 100%; 
+	text-align: center;
+	$[disabled] {
+		opacity: .3;
+	}
+`
+
+export const Error = styled.span`
+	font-size: 14px;
+	color: red;
+	$[disabled] {
+		opacity: .3;
+	}
+`
+
+export const Title = styled.h2`
+	font-size: 16px;
+	font-weight: 500;
+	padding: 8px 0;
+`
--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, {Fragment} from "react";
 import Context from "../Context";
+import {UserForm} from "../components/UserForm";
+import {RegisterMutation} from "../container/RegisterMutation";
 
 export const NotRegisteredUser = () => (
 	<Context.Consumer>
 		{
 			({ activateAuth }) => {
-				return (
-					<form onSubmit={activateAuth}>
-						<button> Login </button>
-					</form>
-				)
+				return <Fragment>
+					<RegisterMutation>
+					{
+						(register, {data, loading, error}) => {
+							const onSubmit = ({email, password}) => {
+								const input = {email, password}
+								const variables = {input}
+								register({ variables}).then(activateAuth)
+							}
+
+							const errorMsg = error &&  "The user is already exists or something went wrong"
+
+							return <UserForm disabled={loading} error={errorMsg} title="Register" onSubmit={onSubmit} /> 
+						}
+					}
+					</RegisterMutation>
+					<UserForm title="Login" onSubmit={activateAuth} />
+				</Fragment>
 			}
 		}
 	</Context.Consumer>
